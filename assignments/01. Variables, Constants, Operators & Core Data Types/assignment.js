cd4281cf72635ecee4b02421e03cc353fb862ca6
vermantjarne <vermant.jarne@gmail.com>
2026-08-06 15:57:55 +0200

    Finish assignment for Variables, Constants, Operators & Core Data Types

---
+++ assignments/01. Variables, Constants, Operators & Core Data Types/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 13;
+let result;
+
+result = userInput + 18;
+
+result *= 3;
+result -= 24;
+result = result % 10;
+
+alert(`De input van de gebruiker was: ${userInput}`);
+alert(`Het resultaat van de berekening is: ${result}`);
